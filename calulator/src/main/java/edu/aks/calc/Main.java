cb5a06b59411b2f6f47421c396c8f6c8e35face6
AakankshaKaskar <akankshakaskar3108@gmail.com>
2020-04-08 14:35:29 -0400

    Log4j + Slf4j added

--- calulator/src/main/java/edu/aks/calc/Main.java
+++ calulator/src/main/java/edu/aks/calc/Main.java
@@ -1,11 +1,17 @@
 package edu.aks.calc;
 
-import edu.aks.calc.util.CommandUtil;
 
-public class Main {
+import edu.aks.calc.util.CommandUtil;
+import lombok.extern.log4j.Log4j;
+
+@Log4j
+public class Main{
+
+   // private static Logger logger = LoggerFactory.getLogger(Main.class);
+
 
     public static void main ( String[] args ) {
-
+        log.info("Main method functionality started !!!!");
         CommandUtil util = new CommandUtil ();
         Integer userIp = util.showInterface ();
         if (userIp == 0) {
