a392c43a52943d9ca5738858b6e56e311fd74897
AakankshaKaskar <akankshakaskar3108@gmail.com>
2020-04-09 01:27:18 -0400

    Log4j + Slf4j added

--- calulator/src/main/java/edu/aks/calc/Main.java
+++ calulator/src/main/java/edu/aks/calc/Main.java
@@ -3,18 +3,37 @@ package edu.aks.calc;
 import edu.aks.calc.util.CommandUtil;
 import lombok.extern.log4j.Log4j;
 
-@Log4j
-public class Main{
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
-    public static void main ( String[] args ) {
+@Log4j
+public class Main {
+
+    public static void main(String[] args) {
         log.info("Main method functionality started !!!!");
-        CommandUtil util = new CommandUtil ();
-        Integer userIp = util.showInterface ();
-        if (userIp == 0) {
-            System.exit ( 0 );
-        } else {
-            util.operatorChoiceMenu ( userIp );
+        try (InputStream input = Main.class.getClassLoader().getResourceAsStream("config.properties")) {
+            Properties prop = new Properties();
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return;
+            }
+            //load a properties file from class path, inside static method
+            prop.load(input);
+
+            //get the property value and print it out
+            log.info("Environment from orperty file is as " + prop.getProperty("env"));
+        } catch (IOException ex) {
+            log.error("Exception is as ::", ex);
+         //   ex.printStackTrace();
         }
-        System.out.println ( "Thanks for visiting !!!" );
+        CommandUtil util = new CommandUtil();
+        Integer userIp = util.showInterface();
+        if (userIp == 0) {
+            System.exit(0);
+        } else {
+            util.operatorChoiceMenu(userIp);
+        }
+        System.out.println("Thanks for visiting !!!");
     }
 }
