3b22460501ed5a9323a32e5aa27ecd23894620ba
aksLearning01 <akanksha.kmit@gmail.com>
2020-06-24 13:49:19 -0400

    Add method for merge conflict test

--- calulator/src/main/java/edu/aks/calc/Main.java
+++ calulator/src/main/java/edu/aks/calc/Main.java
@@ -3,7 +3,9 @@ package edu.aks.calc;
 public class Main {
 
     public static void main ( String[] args ) {
-
+        //Added to test merge conflict functionality
+        Sum sum = new Sum();
+        System.out.println(sum.add(2,4));
 
     }
 }
